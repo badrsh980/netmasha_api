260aedf3941bd80460225ab3c27ef535faf85b38
Bader Al-Harbi <60719639+badrsh980@users.noreply.github.com>
2023-12-26 13:02:32 +0300

    bader

--- bin/handlers/auth/createAccountHandler.dart
+++ bin/handlers/auth/createAccountHandler.dart
@@ -7,12 +7,10 @@ import '../../helper/checkBody.dart';
 createAccountHandler(Request req) async {
   try {
     final Map body = json.decode(await req.readAsString());
-
     List<String> keyNames = ["email", "password", "phone", "name"];
-
     checkBody(body: body, keysCheck: keyNames);
-    AuthResponse? user;
 
+    AuthResponse? user;
     final supabase = SupabaseIntegration.instant;
 
     await supabase!.auth.admin
@@ -22,11 +20,14 @@ createAccountHandler(Request req) async {
     ))
         .then((value) async {
       try {
-        await supabase.auth.resend(type: OtpType.signup, email: body["email"]);
+        await supabase.auth.resend(
+          type: OtpType.signup,
+          email: body["email"],
+        );
         body.remove("email");
         body.remove("phone");
         body.remove("password");
-        final uuid = <String, String>{"uuid_auth": user!.user!.id};
+        final uuid = <String, String>{"uuid_auth": value.user!.id};
         body.addEntries(uuid.entries);
         await supabase.from('explorers').insert(body);
       } catch (error) {
